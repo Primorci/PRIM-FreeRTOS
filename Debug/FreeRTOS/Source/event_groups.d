f85ec248441b68e9304c59a5f9812171e9a1e735
Miha Plibersek <miha.plib@gmail.com>
2025-01-08 22:04:55 +0100

    Initial commit

---
+++ Debug/FreeRTOS/Source/event_groups.d
@@ -0,0 +1,27 @@
+FreeRTOS/Source/event_groups.o: ../FreeRTOS/Source/event_groups.c \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/FreeRTOSConfig.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/portable.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/task.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/list.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/timers.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/task.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/event_groups.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/timers.h
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/FreeRTOS.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/FreeRTOSConfig.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/projdefs.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/portable.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/deprecated_definitions.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/mpu_wrappers.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/task.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/list.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/timers.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/task.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/event_groups.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/timers.h:
---
+++ Debug/FreeRTOS/Source/portable/MemMang/heap_4.d
@@ -0,0 +1,20 @@
+FreeRTOS/Source/portable/MemMang/heap_4.o: \
+ ../FreeRTOS/Source/portable/MemMang/heap_4.c \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/FreeRTOSConfig.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/portable.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/task.h \
+ C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/list.h
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/FreeRTOS.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/FreeRTOSConfig.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/projdefs.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/portable.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/deprecated_definitions.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/mpu_wrappers.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/task.h:
+C:/Users/mihap/STM32CubeIDE/workspace_1.16.1/PRIM-FreeRTOS/FreeRTOS/Source/include/list.h:
